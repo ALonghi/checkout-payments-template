9b714e8e4039bc8cb11a0e32e15f4e0aeec0a047
Alessio Longhi <alonghii@icloud.com>
2022-01-19 20:01:04 +0100

    Add element to center content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ function MyApp({Component, pageProps}: AppProps) {
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                     {/* This element is to trick the browser into centering the modal contents. */}
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                    {/* This element is to trick the browser into centering the modal contents. */}
                     <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4
                     text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                     <Component {...pageProps} />
